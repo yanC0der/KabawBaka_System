287a3b6eebf8cb8c393440147cc57d65c8864c16
Yancy Bien <bienyancynotarion@gmail.com>
2025-11-17 00:08:24 +0800

    cleaned the UI

--- php/navbar.php
+++ php/navbar.php
@@ -14,15 +14,22 @@ echo '      <li><a href="/kabawbaka/index.html">Home</a></li>';
 echo '      <li><a href="/kabawbaka/marketplace.html">Marketplace</a></li>';
 echo '      <li><a href="/kabawbaka/kabawbaka.html">KabawBaka?</a></li>';
 echo '      <li><a href="#about">About</a></li>';
-echo '      <li><a href="#contact">Contact</a></li>';
 echo '    </ul>';
 echo '  </nav>';
 
 echo '  <div id="userMenu">';
 if (!empty($_SESSION['user_id'])) {
     $name = !empty($_SESSION['username']) ? htmlspecialchars($_SESSION['username']) : 'Member';
-    echo "<a href=\"/kabawbaka/user_dashboard.html\" class=\"btn-login\">Dashboard</a>";
-    echo "<a href=\"#\" id=\"logoutBtn\" class=\"btn-login\">Logout</a>";
+    // Check if page parameter is set to dashboard
+    $is_dashboard = isset($_GET['page']) && $_GET['page'] === 'dashboard';
+    
+    if ($is_dashboard) {
+        // Only show Logout on dashboard
+        echo "<a href=\"#\" id=\"logoutBtn\" class=\"btn-login\">Logout</a>";
+    } else {
+        // Show Dashboard link on other pages
+        echo "<a id=\"dashboardLink\" href=\"/kabawbaka/user_dashboard.html\" class=\"btn-login\">Dashboard</a>";
+    }
 } else {
     echo '    <a href="/kabawbaka/login.html" class="btn-login">Login</a>';
     echo '    <a href="/kabawbaka/register.html" class="btn-login">Register</a>';
